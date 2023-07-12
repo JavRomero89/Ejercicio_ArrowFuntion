f770d511e51e4e0107bb20cd94f9f6bbd2faaf60
JavRomero89 <javieromero23@gmail.com>
2023-07-12 18:38:24 +0200

    advance

--- main.js
+++ main.js
@@ -1,38 +1,67 @@
-// Función flecha sin parámetros de entrada
+//Ejercicio 1
 const sinParametros = () => {
-    // Crear variable y asignarle una operación
-    const resultado = 5 + 3;
-  
-    // Mostrar resultado por pantalla
-    console.log(resultado);
-  };
-  
-  sinParametros();
-  
-  // Función flecha con dos parámetros de entrada
-  const dosParametros = (param1, param2) => {
-    // Declarar variables y realizar operaciones
-    const multiplicacion = param1 * param2;
-    const suma = param2 + 10;
-  
-    // Retornar un array con las variables
-    return [multiplicacion, suma];
-  };
-  
-  const resultadoDosParametros = dosParametros(4, 7);
-  console.log(resultadoDosParametros);
-  
-  // Utilizar .map() para obtener todos los números pares en un nuevo array
-  const arrayOriginal = [1, 3, 5, 7];
-  const arrayPares = arrayOriginal.map((num) => num * 2);
-  console.log(arrayPares);
-  
-  // Función flecha sin parámetros que retorna la longitud de un objeto
-  const obtenerLongitud = () => {
-    const objeto = { nombre: "Juan", edad: 25, ciudad: "Madrid" };
-    return Object.keys(objeto).length;
-  };
-  
-  const longitudObjeto = obtenerLongitud();
-  console.log(longitudObjeto);
-  
+
+  const resultado = 5 + 6;
+
+  console.log(resultado);
+}
+sinParametros();
+
+// Ejercicio 2
+const dosParametros = (param1, param2) => {
+
+  const multiplicacion = param1 * param2;
+  const suma = param2 + 10;
+
+  return [multiplicacion, suma];
+}
+
+const resultadoDosParametros = dosParametros(4, 7);
+console.log(resultadoDosParametros);
+
+// Ejercicio 3
+const array = [1, 3, 5, 7];
+const arrayPares = array.map((num) => num * 2);
+console.log(arrayPares);
+
+// Ejercicio 4
+const obtenerLongitud = () => {
+  const objeto = { 
+    nombre: "Juan",
+    edad: 25,
+    ciudad: "Madrid" 
+  }
+  return Object.keys(objeto).length;
+}
+
+const longitudObjeto = obtenerLongitud();
+console.log(longitudObjeto);
+
+//Ejercicio 5
+
+const arrayObj = [
+   {
+    name: "Spain",
+    language: "Spanish"
+  },{
+    name: "Venezuela",
+    language: "Spanish"
+  },{
+    name: "United kingdom",
+    language: "English"
+  },{
+    name: "Nicaragua",
+    language: "Spanish"
+  },{
+    name: "United States",
+    language: "English"
+  },{
+    name: "Iceland",
+    language: "English"
+  },{
+    name: "United Arab Emirates",
+    language: "Arabic"
+  }];
+
+  const hablaHispana = arrayObj.filter((name) => name.language==="Spanish")
+  console.log(hablaHispana);
